206eb00799df382072701cf82101d6a4f6bbd728
soniapathak <sonia.soni04@gmail.com>
2019-11-24 17:16:53 +0100

    random image is picked and select two more breeds

--- src/actions/index.js
+++ src/actions/index.js
@@ -7,22 +7,21 @@ function breedList(payload) {
   }
 }
 
-
 export const updateBreeds = () => (dispatch, getState) => {
-  
-    fetch('https://dog.ceo/api/breeds/list/all')
-      .then(response => response.json())
-      .then(data => {
-        const breeds = Object.keys(data.message)
-        //console.log("breeds list: ", breeds)
-        const action = breedList(breeds)
-  
-        dispatch(action)
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  }
+
+  fetch('https://dog.ceo/api/breeds/list/all')
+    .then(response => response.json())
+    .then(data => {
+      const breeds = Object.keys(data.message)
+      //console.log("breeds list: ", breeds)
+      const action = breedList(breeds)
+
+      dispatch(action)
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+}
 //getBreedimages 
 
 export const GET_BREEDS_IMAGES = 'GET_BREEDS_IMAGES'
@@ -37,16 +36,42 @@ function breedImages(payload) {
 
 export const getBreedimages = (data) => (dispatch, getState) => {
 
-      const breed =   data
-    fetch(`https://dog.ceo/api/breed/${encodeURIComponent(breed)}/images`)
-      .then(response => response.json())
-      .then(responseJSON => {
-        //console.log("response JSON: ", responseJSON)
-        const images = (responseJSON.message.slice(1,11))
-        const action = breedImages(images)
-        dispatch(action)
-        })
-        .catch((error) => {
-          console.error(error);
-        });
-    }
+  const breed = data
+  fetch(`https://dog.ceo/api/breed/${encodeURIComponent(breed)}/images`)
+    .then(response => response.json())
+    .then(responseJSON => {
+      //console.log("response JSON: ", responseJSON)
+      const images = (responseJSON.message.slice(1, 11))
+      const action = breedImages(images)
+      dispatch(action)
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+}
+//getRandomImage
+
+export const GET_RANDOM_IMAGE = 'GET_RANDOM_IMAGE'
+
+function randomImages(payload) {
+  return {
+    type: GET_RANDOM_IMAGE,
+    payload: payload
+  }
+}
+
+
+export const getRandomImage = () => (dispatch, getState) => {
+
+  fetch(`https://dog.ceo/api/breeds/image/random`)
+    .then(response => response.json())
+    .then(responseJSON => {
+      console.log("response of random image ", responseJSON)
+      const action = randomImages(responseJSON.message)
+      dispatch(action)
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+}
+
